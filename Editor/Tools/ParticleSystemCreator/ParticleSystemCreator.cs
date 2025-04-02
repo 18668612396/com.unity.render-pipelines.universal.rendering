e5e286f00fb0e65c4846e5ef9ee67ae8e023a328
世界 <world.18668612396@gmail.com>
2025-04-02 20:40:28 +0800

    添加了一个可以创建初始静止粒子系统的功能

---
+++ Editor/Tools/ParticleSystemCreator/ParticleSystemCreator.cs
@@ -0,0 +1,131 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ParticleSystemCreator : Editor
+{
+    [MenuItem("GameObject/Effects/静止单粒子Mesh", false, 10)]
+    public static void CreateSingleMeshParticleSystem(MenuCommand menuCommand)
+    {
+        //创建一个新的GameObject
+        GameObject go = new GameObject("Single Mesh Particle");
+        //设置父对象,如果当前选择了一个对象,则将新对象设置为当前选择对象的子对象
+        GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+        //确保新对象在层级视图中正确显示
+        Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+        //添加组件
+        var particle = go.AddComponent<ParticleSystem>();
+        //设置粒子系统的属性
+        //1:关闭ShapeModule
+        DisableShapeModule(particle);
+        //设置粒子为单次发射一个
+        SetSinglePointEmission(particle);
+        //设置粒子的速度为0
+        SetSpeedToZero(particle);
+        //设置粒子的渲染模式为Mesh
+        SerRendererMeshFromSphere(particle);
+        //设置粒子的材质
+        SetRendererMaterial(particle);
+        //设置最大粒子数量
+        SetMaxParticles(particle);
+        //设置最大粒子尺寸
+        SetMaxParticleSize(particle);
+        //设置粒子的缩放模式
+        SetScalingMode(particle);
+        //设置粒子的循环模式
+        DisableLooping(particle);
+        //选择当前这个GameObject
+        Selection.activeObject = go;
+    }
+    [MenuItem("GameObject/Effects/静止单粒子Billboard", false, 10)]
+    public static void CreateSingleBillboardParticleSystem(MenuCommand menuCommand)
+    {
+        //创建一个新的GameObject
+        GameObject go = new GameObject("Single Billboard Particle");
+        //设置父对象,如果当前选择了一个对象,则将新对象设置为当前选择对象的子对象
+        GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+        //确保新对象在层级视图中正确显示
+        Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+        //添加组件
+        var particle = go.AddComponent<ParticleSystem>();
+        //设置粒子系统的属性
+        //1:关闭ShapeModule
+        DisableShapeModule(particle);
+        //设置粒子为单次发射一个
+        SetSinglePointEmission(particle);
+        //设置粒子的速度为0
+        SetSpeedToZero(particle);
+        //设置粒子的材质
+        SetRendererMaterial(particle);
+        //设置最大粒子数量
+        SetMaxParticles(particle);
+        //设置最大粒子尺寸
+        SetMaxParticleSize(particle);
+        //设置粒子的缩放模式
+        SetScalingMode(particle);
+        //设置粒子的循环模式
+        DisableLooping(particle);
+        //选择当前这个GameObject
+        Selection.activeObject = go;
+    }
+
+    static void DisableLooping(ParticleSystem particle)
+    {
+        var main = particle.main;
+        main.loop = false;
+    }
+
+    static void SetScalingMode(ParticleSystem particle)
+    {
+        var main = particle.main;
+        main.scalingMode = ParticleSystemScalingMode.Hierarchy;
+    }
+
+    static void SetMaxParticles(ParticleSystem particle)
+    {
+        var main = particle.main;
+        main.maxParticles = 1;
+    }
+
+    static void SetMaxParticleSize(ParticleSystem particle)
+    {
+        var renderer = particle.GetComponent<ParticleSystemRenderer>();
+        renderer.maxParticleSize = 1000;
+    }
+
+    static void DisableShapeModule(ParticleSystem particle)
+    {
+        var shape = particle.shape;
+        shape.shapeType = ParticleSystemShapeType.Circle;
+        shape.radius = 0.001f;
+        shape.enabled = false;
+    }
+
+    static void SetSinglePointEmission(ParticleSystem particle)
+    {
+        var emission = particle.emission;
+        emission.rateOverTime = 0;
+        //设置粒子的发射模式为Burst
+        emission.burstCount = 1;
+        emission.SetBurst(0, new ParticleSystem.Burst(0, 1));
+    }
+
+    static void SetSpeedToZero(ParticleSystem particle)
+    {
+        var main = particle.main;
+        main.startSpeed = 0;
+    }
+
+    private static void SerRendererMeshFromSphere(ParticleSystem particle)
+    {
+        var renderer = particle.GetComponent<ParticleSystemRenderer>();
+        renderer.renderMode = ParticleSystemRenderMode.Mesh;
+        renderer.mesh = Resources.GetBuiltinResource<Mesh>("New-Sphere.fbx");
+        renderer.alignment = ParticleSystemRenderSpace.Local;
+    }
+
+    private static void SetRendererMaterial(ParticleSystem particle)
+    {
+        var renderer = particle.GetComponent<ParticleSystemRenderer>();
+        renderer.material = AssetDatabase.GetBuiltinExtraResource<Material>("Default-Particle.mat");
+    }
+}
